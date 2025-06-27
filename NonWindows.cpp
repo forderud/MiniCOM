a101d24d85b2ebb2d26b4ae6662f1715b02b605b
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-06-27 10:40:13 +0200

    Explicitly annotate cpp-file functions with default symbol visibility, so that the code compiles & links also with hidden symbol visibility by default.

--- NonWindows.cpp
+++ NonWindows.cpp
@@ -1,33 +1,39 @@
 #include "NonWindows.hpp"
 
 
+__attribute__((visibility("default")))
 std::map<std::pair<GUID,std::string>, IUnknownFactory::Factory> & IUnknownFactory::Factories () {
     static std::map<std::pair<GUID,std::string>, Factory> s_factory;
     return s_factory;
 }
 
 
-template <> ATL::CComSafeArray<BSTR>::CComSafeArray (UINT size) {
+template <> __attribute__((visibility("default")))
+ATL::CComSafeArray<BSTR>::CComSafeArray (UINT size) {
     m_ptr.reset(new SAFEARRAY(SAFEARRAY::TYPE_STRINGS));
     m_ptr->strings.resize(size);
 }
-template <> ATL::CComSafeArray<IUnknown*>::CComSafeArray (UINT size) {
+template <> __attribute__((visibility("default")))
+ATL::CComSafeArray<IUnknown*>::CComSafeArray (UINT size) {
     m_ptr.reset(new SAFEARRAY(SAFEARRAY::TYPE_POINTERS));
     m_ptr->pointers.resize(size);
 }
 
-template <> ATL::CComTypeWrapper<BSTR>::type& CComSafeArray<BSTR>::GetAt (int idx) const {
+template <> __attribute__((visibility("default")))
+ATL::CComTypeWrapper<BSTR>::type& CComSafeArray<BSTR>::GetAt (int idx) const {
     assert(m_ptr);
     assert(m_ptr->type == SAFEARRAY::TYPE_STRINGS);
     return m_ptr->strings[idx];
 }
-template <> ATL::CComTypeWrapper<IUnknown*>::type& CComSafeArray<IUnknown*>::GetAt (int idx) const {
+template <> __attribute__((visibility("default")))
+ATL::CComTypeWrapper<IUnknown*>::type& CComSafeArray<IUnknown*>::GetAt (int idx) const {
     assert(m_ptr);
     assert(m_ptr->type == SAFEARRAY::TYPE_POINTERS);
     return m_ptr->pointers[idx];
 }
 
-template <> HRESULT ATL::CComSafeArray<BSTR>::Add (const typename CComTypeWrapper<BSTR>::type& t, BOOL copy) {
+template <> __attribute__((visibility("default")))
+HRESULT ATL::CComSafeArray<BSTR>::Add (const typename CComTypeWrapper<BSTR>::type& t, BOOL copy) {
     (void)copy; // mute unreferenced argument warning
 
     if (!m_ptr)
@@ -37,7 +43,8 @@ template <> HRESULT ATL::CComSafeArray<BSTR>::Add (const typename CComTypeWrappe
     m_ptr->strings.push_back(t);
     return S_OK;
 }
-template <> HRESULT ATL::CComSafeArray<IUnknown*>::Add (const typename CComTypeWrapper<IUnknown*>::type& t, BOOL copy) {
+template <> __attribute__((visibility("default")))
+HRESULT ATL::CComSafeArray<IUnknown*>::Add (const typename CComTypeWrapper<IUnknown*>::type& t, BOOL copy) {
     (void)copy; // mute unreferenced argument warning
 
     if (!m_ptr)
@@ -48,12 +55,14 @@ template <> HRESULT ATL::CComSafeArray<IUnknown*>::Add (const typename CComTypeW
     return S_OK;
 }
 
-template <> unsigned int ATL::CComSafeArray<BSTR>::GetCount () const {
+template <> __attribute__((visibility("default")))
+unsigned int ATL::CComSafeArray<BSTR>::GetCount () const {
     assert(m_ptr);
     assert(m_ptr->type == SAFEARRAY::TYPE_STRINGS);
     return static_cast<unsigned int>(m_ptr->strings.size());
 }
-template <> unsigned int ATL::CComSafeArray<IUnknown*>::GetCount () const {
+template <> __attribute__((visibility("default")))
+unsigned int ATL::CComSafeArray<IUnknown*>::GetCount () const {
     assert(m_ptr);
     assert(m_ptr->type == SAFEARRAY::TYPE_POINTERS);
     return static_cast<unsigned int>(m_ptr->pointers.size());
