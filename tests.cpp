767ba7457c8e32af2cac84f164a11a5b6ae39230
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-14 13:28:42 +0100

    .

--- tests.cpp
+++ tests.cpp
@@ -1,6 +1,21 @@
+#include <cassert>
 #include "NonWindows.hpp"
+
+
+void TestCComSafeArray() {
+    // create numeric SAFEARRAY object with 3 elements
+    CComSafeArray<double> obj(3);
+    {
+        double * vals = &obj.GetAt(0); // will fail if empty
+        vals[0] = 2.0;
+        vals[1] = 3.0;
+        vals[2] = 4.0;
+    }
+    
+    assert(obj.GetCount() == 3u);
+}
 
 int main() {
     printf("Running tests...\n");
-    // TODO: Add actual tests
+    TestCComSafeArray();
 }
