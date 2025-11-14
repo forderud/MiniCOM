7718d404a254257ba85fed759ffbbe43413b6c8d
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-14 13:32:34 +0100

    add failing test

--- tests.cpp
+++ tests.cpp
@@ -1,18 +1,35 @@
 #include <cassert>
+#include <vector>
 #include "NonWindows.hpp"
 
 
-void TestCComSafeArray() {
-    // create numeric SAFEARRAY object with 3 elements
-    CComSafeArray<double> obj(3);
-    {
-        double * vals = &obj.GetAt(0); // will fail if empty
-        vals[0] = 2.0;
-        vals[1] = 3.0;
-        vals[2] = 4.0;
+/** Convert raw array to SafeArray. */
+template <class T>
+CComSafeArray<T> ConvertToSafeArray (const T * input, size_t element_count) {
+    CComSafeArray<T> result(static_cast<ULONG>(element_count));
+
+    if (element_count > 0) {
+        T * out_ptr = &result.GetAt(0); // will fail if empty
+        for (size_t i = 0; i < element_count; ++i)
+            out_ptr[i] = input[i];
     }
-    
-    assert(obj.GetCount() == 3u);
+    return result;
+}
+
+
+void TestCComSafeArray() {
+    std::vector<double> vals = {2.0, 3.0, 4.0};
+    {
+        printf("direct assigmnent...\n");
+        CComSafeArray<double> sa_vals = ConvertToSafeArray(vals.data(), vals.size());
+        assert(sa_vals.GetCount() == 3);
+    }
+    {
+        printf("copy-assignment...\n");
+        CComSafeArray<double> sa_vals;
+        sa_vals = ConvertToSafeArray(vals.data(), vals.size());
+        assert(sa_vals.GetCount() == 3);
+    }
 }
 
 int main() {
