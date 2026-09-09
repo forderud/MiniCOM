6b436df83e27f926931d26ca7e0f6431013b4371
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-09-09 10:16:52 +0200

    Add assertions to python test script
    Based on https://github.com/forderud/MiniCOM/pull/

--- PyTests/tests.py
+++ PyTests/tests.py
@@ -4,10 +4,13 @@ calc = PyTests.CreateCalculator()
 
 val = calc.GetValue()
 print("calc.GetValue() returned "+str(val))
+assert val == 42
 
 val = calc.Add(1,2)
 print("calc.Add(1, 2) returned "+str(val))
+assert val == 3
 
 calc2 = calc.QueryInterface(PyTests.ICalc2)
 val = calc2.GetValue2()
-print("calc2.GetValue() returned "+str(val))
+print("calc2.GetValue2() returned "+str(val))
+assert val == 43
